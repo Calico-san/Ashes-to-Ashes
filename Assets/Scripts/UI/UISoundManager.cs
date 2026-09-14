f1ccfc167fc9ab3afb9be7a59aed2e32442a2c86
mislavBa <mislav.balaz@gmail.com>
2026-09-14 22:38:21 +0200

    Dodan audio manager

---
+++ Assets/Scripts/UI/UISoundManager.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class UISoundManager : MonoBehaviour
+{
+    public static UISoundManager Instance;
+
+    [Header("Audio Clips")]
+    public AudioClip buttonClickClip;
+    public AudioClip panelOpenClip;
+    public AudioClip panelCloseClip;
+
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        // Simple Singleton pattern so you can call this from anywhere
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    
+    public void PlayButtonClick()
+    {
+        PlaySound(buttonClickClip);
+    }
+
+    
+    public void PlayPanelOpen()
+    {
+        PlaySound(panelOpenClip);
+    }
+
+    
+    public void PlayPanelClose()
+    {
+        PlaySound(panelCloseClip);
+    }
+
+    // Utility method using PlayOneShot to allow overlapping sounds
+    public void PlaySound(AudioClip clip)
+    {
+        if (clip != null)
+        {
+            audioSource.PlayOneShot(clip);
+        }
+    }
+}
